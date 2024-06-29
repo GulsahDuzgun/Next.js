ad528b3cece2b934aec9a4b28f4edfc468b46d57
GulsahDuzgun <duzgun.gulsah27@gmail.com>
2024-06-29 15:19:44 +0300

    Creating a Layout

--- 02 - The Wild Oasis Website/app/cabins/page.js
+++ 02 - The Wild Oasis Website/app/cabins/page.js
@@ -1,10 +1,7 @@
-import Navigation from "../components/Navigation";
-
 export default function Page() {
   return (
     <div>
       <h2>cabins</h2>
-      <Navigation />
     </div>
   );
 }
--- 02 - The Wild Oasis Website/app/page.js
+++ 02 - The Wild Oasis Website/app/page.js
@@ -1,10 +1,7 @@
-import Navigation from "./components/Navigation";
-
 export default function Page() {
   return (
     <div>
       <h2>Hello Next.js</h2>
-      <Navigation />
     </div>
   );
 }
