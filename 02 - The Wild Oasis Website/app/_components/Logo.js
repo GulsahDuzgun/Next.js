b22563188deb2d4e091bc6fc90aeb3c1eb00a696
GulsahDuzgun <duzgun.gulsah27@gmail.com>
2024-07-01 20:54:12 +0300

    Optimizing Images With Next.js <Image/> Component
    Image tag in Next.js is provides us to lazy loading and responsive size and many optimization features on images to load app much faster.

--- 02 - The Wild Oasis Website/app/_components/Logo.js
+++ 02 - The Wild Oasis Website/app/_components/Logo.js
@@ -1,9 +1,19 @@
 import Link from "next/link";
+import Image from "next/image";
+import imageFile from "@/public/logo.png";
 
 function Logo() {
   return (
     <Link href="/" className="flex items-center gap-4 z-10">
-      <img height="60" width="60" alt="The Wild Oasis logo" src="./logo.png" />
+      {/* <Image height="60" width="60" alt="The Wild Oasis logo" src="/logo.png" /> */}
+      <Image
+        src={imageFile}
+        height="60"
+        width="60"
+        alt="The Wild Oasis logo"
+        quality={100}
+      />
+
       <span className="text-xl font-semibold text-primary-100">
         The Wild Oasis
       </span>
