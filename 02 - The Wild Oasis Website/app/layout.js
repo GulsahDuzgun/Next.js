b497b2791d6c6dfa820a3dca01db0e71c7275e5d
GulsahDuzgun <duzgun.gulsah27@gmail.com>
2024-07-01 19:20:09 +0300

    Loading and Optimizing Fonts

--- 02 - The Wild Oasis Website/app/layout.js
+++ 02 - The Wild Oasis Website/app/layout.js
@@ -3,6 +3,8 @@ import Navigation from "@/app/_components/Navigation";
 
 // main layout of app --> So global style is okey to use
 import "@/app/_styles/globals.css";
+import { Josefin_Sans } from "next/font/google";
+
 export const metadata = {
   // title: "Keep Going Forward || The Wield Oasis",
   title: {
@@ -13,10 +15,18 @@ export const metadata = {
     "hotel, located in the heart of Italian Dolomites, surrounded by beautiful mountains and dark forest",
 };
 
+const josefin_Font = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
+console.log(josefin_Font);
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-primary-950 text-primary-100 min-h-screen">
+      <body
+        className={`${josefin_Font.className} bg-primary-950 text-primary-100 min-h-screen`}
+      >
         <header>
           <Logo />
           <Navigation />
