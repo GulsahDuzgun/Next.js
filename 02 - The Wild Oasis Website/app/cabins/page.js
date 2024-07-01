d98e281d8d8b226f1bd6523d811d182223199383
GulsahDuzgun <duzgun.gulsah27@gmail.com>
2024-07-01 17:23:46 +0300

    Project Organization

--- 02 - The Wild Oasis Website/app/cabins/page.js
+++ 02 - The Wild Oasis Website/app/cabins/page.js
@@ -1,20 +1,11 @@
-import Counter from "../components/Counter";
+import Counter from "@/app/_components/Counter";
 
-export default async function Page() {
-  //top level async on server
-  const res = await fetch(`https://jsonplaceholder.typicode.com/users`);
-
-  const data = await res.json();
-  console.log(data);
+export default function Page() {
   return (
     <div>
       <h2>cabins</h2>
-      <ul>
-        {data.map((item) => (
-          <li key={item.id}>{item.name}</li>
-        ))}
-      </ul>
-      <Counter users={data} />
+
+      <Counter />
     </div>
   );
 }
