aa6a50336e3e58712ea9b086d5be1793dd35bff2
GulsahDuzgun <duzgun.gulsah27@gmail.com>
2024-07-10 09:37:15 +0300

    Creating a New Guest on First Sign In

--- 02 - The Wild Oasis Website/app/account/page.js
+++ 02 - The Wild Oasis Website/app/account/page.js
@@ -8,6 +8,7 @@ export default async function Page() {
   const session = await auth();
   console.log(session);
   const firstName = session.user.name.split(" ").at(0);
+
   return (
     <h2 className="font-semibold text-2xl text-accent-400 mb-7">
       Hello {firstName}, Keep Going
