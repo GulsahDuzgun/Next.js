794817508c5df4d2c2d5d3127d17dd240b9c0355
GulsahDuzgun <duzgun.gulsah27@gmail.com>
2024-07-11 09:06:05 +0300

    Displaying a Loading indicator: The useFormStatus Hook
    It must be rendered in a component that component inside a form to use useFormStatus() hook in Next.js

--- 02 - The Wild Oasis Website/app/_components/UpdateProfileForm.js
+++ 02 - The Wild Oasis Website/app/_components/UpdateProfileForm.js
@@ -1,5 +1,6 @@
 "use client";
 import React, { useState } from "react";
+import { useFormStatus } from "react-dom";
 import { updateGuest } from "../_lib/actions";
 
 export default function UpdateProfileForm({ children, guest }) {
@@ -54,10 +55,23 @@ export default function UpdateProfileForm({ children, guest }) {
       </div>
 
       <div className="flex justify-end items-center gap-6">
-        <button className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300">
-          Update profile
-        </button>
+        <Button />
       </div>
     </form>
+  );
+}
+
+function Button() {
+  //For using useFormStatus we need to a component inside a form
+
+  const { pending } = useFormStatus();
+
+  return (
+    <button
+      className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300"
+      disabled={pending}
+    >
+      {pending ? "Updating..." : "Update profile"}
+    </button>
   );
 }
