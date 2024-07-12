15c93b748c8c88ed1fe0e547bf9e2f126f0dca8c
GulsahDuzgun <duzgun.gulsah27@gmail.com>
2024-07-12 08:46:11 +0300

    Fetching Data With getStaticProps (SSG)
    fetch static data with getStaticProps

--- 03 - Pages Router/next.config.mjs
+++ 03 - Pages Router/next.config.mjs
@@ -5,7 +5,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "https://gabbeqyyjjkdxvqxuqsn.supabase.co",
+        hostname: "gabbeqyyjjkdxvqxuqsn.supabase.co",
         port: "",
         pathname: "/storage/v1/object/public/cabin-images/**",
       },
