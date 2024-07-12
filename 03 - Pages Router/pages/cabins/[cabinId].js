46ecdd37a0bed2c31e0699a1970bf292fbeb9101
GulsahDuzgun <duzgun.gulsah27@gmail.com>
2024-07-12 08:31:53 +0300

    Defining Page Title and Favicon

--- 03 - Pages Router/pages/cabins/[cabinId].js
+++ 03 - Pages Router/pages/cabins/[cabinId].js
@@ -1,6 +1,14 @@
+import Head from "next/head";
 import { useRouter } from "next/router";
 
 export default function Page() {
   const router = useRouter();
-  return <div>Cabin #{router.query.cabinId}</div>;
+  return (
+    <>
+      <Head>
+        <title>Cabin {router.query.cabinId}</title>
+      </Head>
+      <div>Cabin #{router.query.cabinId}</div>;
+    </>
+  );
 }
